f3fdee19634725c680de0ac1ab75b2ed44af4ae5
Ahmad <ahmad.esmaile10@gmaill.com>
2024-11-04 11:57:32 +0330

    Add Start Up

---
+++ src/CleanDDDCQRS.API/Startup.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using MediatR;
+using CleanDDDCQRS.Infrastructure;
+using CleanDDDCQRS.Domain;
+
+namespace CleanDDDCQRS
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //// تنظیم DbContext
+            //services.AddDbContext<ApplicationDbContext>(options =>
+            //    options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            // ثبت Repository
+            services.AddScoped<ICustomerRepository, CustomerRepository>();
+
+            // ثبت MediatR
+            services.AddMediatR(typeof(Startup)); // اگر Startup در لایه Application باشد
+
+            // ثبت کنترلرها
+            services.AddControllers();
+
+            // تنظیمات دیگر (مثل Swagger، اگر نیاز دارید)
+            services.AddSwaggerGen();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            // تنظیم Swagger
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CleanDDDCQRS API v1"));
+        }
+    }
+}
